8fbf880fae93d4bd7548c1f71aab09a62a1eab6e
Andrei Schneider Majada <andrei.majada@softdesign.com.br>
2021-12-20 16:47:16 -0300

    creating server and adding dependencies

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from "dotenv";
+
+dotenv.config({})
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+  console.info(`Server listening at port ${process.env.PORT}.`);
+});
+
+export default app;
